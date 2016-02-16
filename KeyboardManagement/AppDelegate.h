fad1da1c500df82481826ede8e67892f5f62fc1e
Galileo Guzmán <galileoguzman@gmail.com>
2016-02-16 14:19:23 -0600

    Initial Commit

---
+++ KeyboardManagement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyboardManagement
+//
+//  Created by Galileo Guzman on 16/02/16.
+//  Copyright © 2016 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeyboardManagement/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyboardManagement
+//
+//  Created by Galileo Guzman on 16/02/16.
+//  Copyright © 2016 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
