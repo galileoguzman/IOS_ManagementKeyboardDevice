dc75229df84a80ae3fa982a919878f4d5dad09fa
Galileo Guzmán <galileoguzman@gmail.com>
2016-02-16 14:55:53 -0600

    Management return key

--- KeyboardManagement/Home.h
+++ KeyboardManagement/Home.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "IQKeyboardManager.h"
+#import "IQKeyboardReturnKeyHandler.h"
 
 @interface Home : UIViewController
 
--- KeyboardManagement/Home.m
+++ KeyboardManagement/Home.m
@@ -14,9 +14,13 @@
 
 @implementation Home
 
+IQKeyboardReturnKeyHandler *returnKeyHandler;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    returnKeyHandler = [[IQKeyboardReturnKeyHandler alloc] initWithViewController:self];
 }
 
 - (void)didReceiveMemoryWarning {
